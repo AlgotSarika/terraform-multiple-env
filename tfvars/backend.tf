02daaab1acefb5960183a240a45794f89a0c998d
Algot Sarika <sarika@joindevops.com>
2025-02-07 20:21:14 +0530

    multiple-env

--- tfvars/backend.tf
+++
@@ -1,5 +0,0 @@
-bucket = "45s-tf-remote-state-prod"
-key = "expense-infra-prod" 
-region = "us-east-1"
-dynamodb_table = "45s-tf-remote-state-prod"
-  
--- tfvars/dev/backend.tf
+++ tfvars/dev/backend.tf
@@ -1,6 +1,6 @@
 
-    bucket = "45s-tf-remote-state-prod
-    key = "expense-infra-prodwe should have unique keys with in the bucket,same key should not be used in other repos or tf projects
+    bucket = "45s-tf-remote-state-dev"
+    key = "expense-infra-prod"  #we should have unique keys with in the bucket,same key should not be used in other repos or tf projects
     region = "us-east-1"
-    dynamodb_table = "45s-tf-remote-state-prod
+    dynamodb_table = "45s-tf-remote-state-prod"
   
